0a7232af6b0eed3aa11c1bdaf70a899aa4115ecf
Abu athman <athman.abu@gmail.com>
2024-02-10 15:58:35 +0300

    make links work

--- src/app/_components/SideSocialIcons.tsx
+++ src/app/_components/SideSocialIcons.tsx
@@ -1,15 +1,33 @@
 import { Linkedin, Youtube, Github } from "lucide-react";
 import { BsTwitterX } from "react-icons/bs";
+import Link from "next/link";
 
 type Props = {};
 
 const SideSocialIcons = (props: Props) => {
   return (
     <div className="fixed flex h-full flex-col items-center justify-end gap-4 px-2">
-      <Linkedin size={23} />
-      <Youtube size={23} />
-      <BsTwitterX size={20} />
-      <Github size={23} />
+      <Link href="https://www.linkedin.com/in/abubakar-athman">
+        <Linkedin
+          className="hover:cursor-pointer hover:text-[#0A66C2]"
+          size={23}
+        />
+      </Link>
+      <Link href="https://twitter.com/iathman83">
+        <BsTwitterX
+          className="hover:cursor-pointer hover:text-black"
+          size={20}
+        />
+      </Link>
+      <Link href="https://www.youtube.com/@programwithabu">
+        <Youtube
+          className="hover:cursor-pointer hover:text-[#FF0000]"
+          size={23}
+        />
+      </Link>
+      <Link href="https://github.com/iAthman83">
+        <Github className="hover:cursor-pointer hover:text-black" size={23} />
+      </Link>
       <div className="mt-4 h-2/5 border-l-2 border-l-primary-foreground"></div>
     </div>
   );
